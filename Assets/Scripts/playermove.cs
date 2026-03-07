6a6e8ee35d5020490bea7d7728ef17e891bf7b05
JusoPie <128028155+JusoPie@users.noreply.github.com>
2026-03-07 12:28:59 +0200

    rotation working

--- Assets/Scripts/playermove.cs
+++ Assets/Scripts/playermove.cs
@@ -3,18 +3,24 @@ using System.Collections;
 
 public class playermove : MonoBehaviour
 {
-    public float speed = 10f;
+    public Transform model;
+
+    [SerializeField] public float speed = 10f;
+
+    public float maxTilt = 20f;
+    public float tiltSpeed = 5f;
+
+    float currentTilt;
 
     private Rigidbody rb;
 
-    private SkinnedMeshRenderer smr;
 
     //public Transform lookTarget;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        smr = GetComponent<SkinnedMeshRenderer>();
+
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,27 +33,14 @@ public class playermove : MonoBehaviour
 
     void Update()
     {
-        //transform.LookAt(lookTarget);
+        float input = Input.GetAxis("Horizontal"); // A/D
 
-        //Vector3 difference = lookTarget.position - transform.position;
-        //float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ - 90f);
+        float targetTilt = -input * maxTilt;
 
-        //transform.Rotate(0f, 180f, 0f, Space.Self);
+        currentTilt = Mathf.Lerp(currentTilt, targetTilt, Time.deltaTime * tiltSpeed);
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(-Vector2.right * speed);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(-Vector2.up * speed);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(Vector2.right * speed);
-        }
+        transform.localRotation = Quaternion.Euler(0f, 0f, currentTilt);
     }
+
+
 }
